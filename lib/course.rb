ba4c960c4cea73a63d320f9b99dd0f2e2a564b3e
Chris Junker <elevatemedfit@gmail.com>
2019-07-24 13:13:29 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,4 +2,18 @@ class Course
 
   attr_accessor :title, :schedule, :description
 
+  @@all =[]
+
+  def initialized
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.reset.all
+    @@all.clear
+  end
+
 end
